4b6cd91edff42c4e3c6af9f27ed3c9c4bf8c2c49
Kerollmops <clement@meilisearch.com>
2023-09-03 16:21:43 +0200

    First version of the website

--- src/bin/search.rs
+++ src/bin/search.rs
@@ -1,15 +1,23 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::path::PathBuf;
+use std::sync::Arc;
 use std::time::Instant;
-use std::{fs, io};
+use std::{fs, include_bytes};
 
+use askama::Template;
+use axum::extract::{Query, State};
+use axum::http::header;
+use axum::response::{IntoResponse, Redirect};
+use axum::routing::get;
+use axum::Router;
 use clap::Parser;
 use croissantine::database::Database;
 use croissantine::encode_trigram;
 use croissantine::text::cleanup_chars;
 use croissantine::text::trigrams::TriGrams;
 use heed::EnvOpenOptions;
-use roaring::{MultiOps, RoaringBitmap};
+use roaring::MultiOps;
+use url::Url;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -19,46 +27,112 @@ struct Options {
     database_path: PathBuf,
 }
 
-fn main() -> anyhow::Result<()> {
+struct AppState {
+    database: Database,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let Options { database_path } = Options::parse();
 
     let mut options = EnvOpenOptions::new();
     options.map_size(100 * 1024 * 1024 * 1024); // 100GiB
     fs::create_dir_all(&database_path)?;
     let database = Database::open_or_create(options, database_path)?;
+    let app_state = Arc::new(AppState { database });
 
-    let lines = io::stdin().lines();
-    for result in lines {
-        let line = result?;
-        let before = Instant::now();
-        let trigrams: Vec<_> = TriGrams::new(cleanup_chars(line.chars())).collect();
-        let trigrams = &trigrams[1..trigrams.len() - 1];
-        let rtxn = database.read_txn()?;
-        let mut title_trigram_bitmaps = Vec::new();
-        let mut content_trigram_bitmaps = Vec::new();
-        let mut key = String::new();
-        for trigram in trigrams {
-            let key = encode_trigram(&mut key, *trigram);
-            if let Some(bitmap) = database.title_ngrams_docids.get(&rtxn, key)? {
-                title_trigram_bitmaps.push(bitmap);
-            }
-            if let Some(bitmap) = database.content_ngrams_docids.get(&rtxn, key)? {
-                content_trigram_bitmaps.push(bitmap);
-            }
-        }
+    // our router
+    let app = Router::new()
+        .route("/", get(welcome))
+        .route("/search", get(search))
+        .route("/about", get(about))
+        .route("/assets/images/croissantine-logo.svg", get(assets_images_logo))
+        .with_state(app_state);
 
-        let title_bitmap = title_trigram_bitmaps.intersection();
-        let mut content_bitmap = content_trigram_bitmaps.intersection();
-        content_bitmap -= &title_bitmap;
-
-        for (i, docid) in title_bitmap.into_iter().chain(content_bitmap).take(20).enumerate() {
-            if let Some(url) = database.docid_uri.get(&rtxn, &docid)? {
-                println!("{i:>3}. {url}");
-            }
-        }
-
-        eprintln!("Searching for `{}` took {:.02?}", line, before.elapsed());
-    }
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap()).serve(app.into_make_service()).await?;
 
     Ok(())
+}
+
+#[derive(Template)]
+#[template(path = "welcome.html")]
+struct WelcomeTemplate;
+
+async fn welcome() -> WelcomeTemplate {
+    WelcomeTemplate
+}
+
+#[derive(Template)]
+#[template(path = "results.html")]
+struct ResultsTemplate {
+    time_taken: String,
+    count: u64,
+    query: String,
+    results: Vec<Result>,
+}
+
+struct Result {
+    link: Url,
+    title: String,
+}
+
+async fn search(
+    State(state): State<Arc<AppState>>,
+    Query(params): Query<HashMap<String, String>>,
+) -> impl IntoResponse {
+    let database = &state.database;
+    let query = match params.get("query").cloned() {
+        Some(query) if !query.is_empty() => query,
+        _ => return Box::new(Redirect::temporary("/")).into_response(),
+    };
+
+    let before = Instant::now();
+    let trigrams: Vec<_> = TriGrams::new(cleanup_chars(query.chars())).collect();
+    let trigrams = &trigrams[1..trigrams.len() - 1];
+    let rtxn = database.read_txn().unwrap();
+    let mut title_trigram_bitmaps = Vec::new();
+    let mut content_trigram_bitmaps = Vec::new();
+    let mut key = String::new();
+    for trigram in trigrams {
+        let key = encode_trigram(&mut key, *trigram);
+        if let Some(bitmap) = database.title_ngrams_docids.get(&rtxn, key).unwrap() {
+            title_trigram_bitmaps.push(bitmap);
+        }
+        if let Some(bitmap) = database.content_ngrams_docids.get(&rtxn, key).unwrap() {
+            content_trigram_bitmaps.push(bitmap);
+        }
+    }
+
+    let title_bitmap = title_trigram_bitmaps.intersection();
+    let mut content_bitmap = content_trigram_bitmaps.intersection();
+    content_bitmap -= &title_bitmap;
+    let count = title_bitmap.union_len(&content_bitmap);
+
+    let mut results = Vec::new();
+    for docid in title_bitmap.into_iter().chain(content_bitmap).take(20) {
+        if let Some(url) = database.docid_uri.get(&rtxn, &docid).unwrap() {
+            let title = url.to_string();
+            let link = Url::parse(url).unwrap();
+            results.push(Result { link, title });
+        }
+    }
+
+    eprintln!("Searching for `{}` took {:.02?}", query, before.elapsed());
+    Box::new(ResultsTemplate {
+        time_taken: format!("{:.02?}", before.elapsed()),
+        count,
+        query,
+        results,
+    })
+    .into_response()
+}
+
+async fn about() -> Redirect {
+    Redirect::temporary("https://github.com/Kerollmops/croissantine")
+}
+
+async fn assets_images_logo() -> impl IntoResponse {
+    let bytes = include_bytes!("../../assets/images/croissantine-logo.svg");
+    ([(header::CONTENT_TYPE, "image/svg+xml")], bytes)
 }
